9c7cb67c7b4324b8bcbbb5536bee9528c9ee891f
engineeringwitharavind <webdev.aravind@gmail.com>
2023-01-22 18:28:42 +0530

    feat: Add Location form

---
+++ src/components/AddLocation/AddLocation.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { TextField, Button } from "@material-ui/core";
+import SearchableMap from "../SearchableMap/SearchableMap";
+
+interface FormState {
+  name: string;
+  description: string;
+}
+
+function AddLocation() {
+  const [formState, setFormState] = React.useState<FormState>({
+    name: "",
+    description: "",
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("[addLocation.handleSubmit.formState]", formState);
+  };
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Name"
+          name="name"
+          value={formState.name}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+        <br />
+        <TextField
+          label="Description"
+          name="description"
+          value={formState.description}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+          multiline
+        />
+        <br />
+        <Button type="submit" variant="contained" color="primary">
+          Save
+        </Button>
+      </form>
+      <SearchableMap />
+    </React.Fragment>
+  );
+}
+
+export default AddLocation;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,14 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import "./App.css";
-import SearchableMap from "../SearchableMap/SearchableMap";
+import AddLocation from "../AddLocation/AddLocation";
 
-export const MAPBOX_TOKEN =
-  "pk.eyJ1IjoiYXJhdmluZC1reXJvIiwiYSI6ImNsZDJ2bzNrazAwNWUzbm50eDFmNnBiangifQ.qORN4LTWUeVy1nzj_l1Bgw";
+export const MAPBOX_TOKEN = "";
 
 function App() {
   return (
     <div className="App">
-      <SearchableMap />
+      <AddLocation />
     </div>
   );
 }
