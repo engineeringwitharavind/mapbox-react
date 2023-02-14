88229ca24ee9eb27627f3ad092e24b8fbdf3177e
engineeringwitharavind <webdev.aravind@gmail.com>
2023-02-14 19:46:48 +0530

    feat: update plot polygon poc

--- src/components/SearchableMap/PlotMap.tsx
+++ src/components/SearchableMap/PlotMap.tsx
@@ -1,64 +1,74 @@
-import { useState, useCallback } from "react";
-import Map from "react-map-gl";
+import { useState, useRef } from "react";
+import Map, { Source, Layer, MapboxMap } from "react-map-gl";
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import { MAPBOX_TOKEN } from "../App/App";
-import DrawControl from "./DrawControl";
 
-interface Feature {
-  id: string;
-}
+const DrawPolygonMap = () => {
+  const mapRef = useRef<MapboxMap | null>(null);
 
-interface Features {
-  [key: string]: Feature;
-}
+  const [features, setFeatures] = useState<any>({
+    type: "FeatureCollection",
+    features: [],
+  });
+  const drawRef = useRef<any>(null);
 
-function PlotMap() {
-  const [, setFeatures] = useState({});
-
-  const onUpdate = useCallback((e: { features: any }) => {
-    setFeatures((currFeatures: Features) => {
-      const newFeatures: Features = { ...currFeatures };
-      for (const f of e.features) {
-        newFeatures[f.id] = f;
-      }
-      return newFeatures;
-    });
-  }, []);
-
-  const onDelete = useCallback((e: { features: any }) => {
-    setFeatures((currFeatures: Features) => {
-      const newFeatures: Features = { ...currFeatures };
-      for (const f of e.features) {
-        delete newFeatures[f.id];
-      }
-      return newFeatures;
-    });
-  }, []);
+  const onDrawCreateOrUpdate = () => {
+    const data = drawRef.current.getAll();
+    setFeatures(data);
+    if (data.features.length) {
+      const coordinates = data.features[0].geometry.coordinates;
+      console.log("[PlotMap.onDrawCreateOrUpdate().coordinates]", coordinates);
+    }
+  };
 
   return (
     <Map
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      mapboxAccessToken={MAPBOX_TOKEN}
       initialViewState={{
         longitude: -91.874,
         latitude: 42.76,
         zoom: 12,
       }}
-      mapStyle="mapbox://styles/mapbox/streets-v12"
-      mapboxAccessToken={MAPBOX_TOKEN}
       style={{ width: 700, height: 500 }}
+      ref={(ref) => {
+        if (ref && !mapRef.current) {
+          mapRef.current = ref.getMap();
+          drawRef.current = new MapboxDraw({
+            displayControlsDefault: false,
+            controls: {
+              polygon: true,
+              trash: true,
+            },
+          });
+          mapRef.current.addControl(drawRef.current, "top-left");
+          mapRef.current.on("draw.create", onDrawCreateOrUpdate);
+          mapRef.current.on("draw.delete", onDrawCreateOrUpdate);
+          mapRef.current.on("draw.update", onDrawCreateOrUpdate);
+        }
+      }}
     >
-      <DrawControl
-        position="top-left"
-        displayControlsDefault={false}
-        controls={{
-          polygon: true,
-          trash: true,
-        }}
-        defaultMode="draw_polygon"
-        onCreate={onUpdate}
-        onUpdate={onUpdate}
-        onDelete={onDelete}
-      />
+      <Source id="draw-polygon" type="geojson" data={features}>
+        <Layer
+          id="draw-polygon-fill"
+          type="fill"
+          paint={{
+            "fill-color": "#088",
+            "fill-opacity": 0.4,
+          }}
+        />
+        <Layer
+          id="draw-polygon-stroke"
+          type="line"
+          paint={{
+            "line-color": "#088",
+            "line-width": 2,
+          }}
+        />
+      </Source>
     </Map>
   );
-}
+};
 
-export default PlotMap;
+export default DrawPolygonMap;
